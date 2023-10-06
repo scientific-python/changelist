f1e2d0130e78a2f08fced50a48dad63c277fef29
Jarrod Millman <jarrod.millman@gmail.com>
2023-10-06 06:50:53 -0700

    Use main function

--- .tools/readme.py
+++ .tools/readme.py
@@ -1,20 +1,25 @@
 import re
 
-with open("README.md") as fh:
-    readme = fh.read()
 
-with open("src/changelist/default_config.toml") as fh:
-    default_config = fh.read()
+def main():
+    with open("README.md") as fh:
+        readme = fh.read()
 
-config_begin = r"<!--- begin default_config.toml --->\n"
-config_end = r"<!--- end default_config.toml --->\n"
-config_section = (
-    config_begin + r"\n````toml\n" + default_config + r"````\n\n" + config_end
-)
+    with open("src/changelist/default_config.toml") as fh:
+        default_config = fh.read()
 
-rx = re.compile(config_begin + ".*?" + config_end, re.DOTALL)
-readme = rx.sub(config_section, readme)
+    config_begin = r"<!--- begin default_config.toml --->\n"
+    config_end = r"<!--- end default_config.toml --->\n"
+    config_section = (
+        config_begin + r"\n````toml\n" + default_config + r"````\n\n" + config_end
+    )
+
+    rx = re.compile(config_begin + ".*?" + config_end, re.DOTALL)
+    readme = rx.sub(config_section, readme)
+
+    with open("README.md", "w") as fh:
+        fh.write(readme)
 
 
-with open("README.md", "w") as fh:
-    fh.write(readme)
+if __name__ == "__main__":
+    main()
